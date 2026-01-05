da4736e1f5bf6766047bd62757c8736e01ea4a6d
akhilmunnuru26 <munnuruakhil26@gmail.com>
2026-01-05 13:05:06 +0530

    Added Login Form component,Login route

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+// import { useAuth } from "../../context/AuthContext";
+// import { useNavigate } from "react-router-dom";
+
+const LoginForm = () => {
+//   const { login } = useAuth();
+//   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+//   const [error, setError] = useState("");
+
+
+  const submit = () => {
+    // if (!login(email, password)) {
+    //   setError("Invalid credentials");
+    // } else {
+    //   navigate("/");
+    // }
+    console.log("Email", email);
+    console.log("Password", password);
+    
+  };
+
+  return (
+    <div className="card">
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={e => setPassword(e.target.value)}
+      />
+      {/* {error && <p className="error">{error}</p>} */}
+      <button onClick={submit}>Login</button>
+    </div>
+  );
+}
+
+
+export default LoginForm;
